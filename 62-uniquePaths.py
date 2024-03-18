40cb537bee5fc9fe1fbaf8dfa800dd7b0a4dcdb9
Mikhail Kalmykov <mikhail.kalmykov@best2pay.net>
2024-03-18 08:34:12 +0300

    62. Unique Paths (Medium) - iterative

--- 62-uniquePaths.py
+++ 62-uniquePaths.py
@@ -1,26 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        dp = [ [ None for _ in range(n+1) ] for _ in range(m+1)]
+        dp = [ 1 for _ in range(n)]
 
-        def f(i: int, j: int) -> int:
-            if i == 1 or j == 1:
-                return 1
-            
-            if dp[i-1][j] == None:
-                s = f(i-1, j)
-            else:
-                s = dp[i-1][j]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j-1]
 
-            if dp[i][j-1] == None:
-                s += f(i, j-1)
-            else:
-                s += dp[i][j-1]
-            
-            dp[i][j] = s
-            return s
-
-        return f(m, n)
+        return dp[-1]
     
 print(Solution().uniquePaths(m = 3, n = 7))
 print(Solution().uniquePaths(m = 3, n = 2))
